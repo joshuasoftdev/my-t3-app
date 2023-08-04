549b1bc8d0c7dfaad0417aa4ef5930149a3a1e76
joshuasoftdev <123251561+joshuasoftdev@users.noreply.github.com>
2023-08-04 01:45:32 -0700

    another commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Header: React.FC = () => (
-  <header>
-    <h1>My T3 App</h1>
+  <header className="header fixed h-20 w-full items-center text-center">
+    <h1 className="p-3 text-5xl text-white">My T3 App</h1>
   </header>
 );
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import Header from "~/components/Header";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,6 +11,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Header />
       <Component {...pageProps} />
     </SessionProvider>
   );
