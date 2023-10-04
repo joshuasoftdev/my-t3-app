89ca5a242333d22895ce2fca1003eb41dfbf8b42
joshuasoftdev <123251561+joshuasoftdev@users.noreply.github.com>
2023-10-03 19:42:48 -0700

    move up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,7 +27,7 @@ const Header: React.FC = () => (
           alt="Logo"
           className="sober ml-20 hidden h-20 w-32 pb-4 lg:block xl:block"
         />
-        <div className="container mx-auto flex flex-col items-center justify-center gap-12 xl:block">
+        <div className="mx-auto pt-2 text-5xl text-white xl:ml-32">
           <h1
             className="mx-auto pt-2 text-5xl text-white xl:ml-32"
             style={{
